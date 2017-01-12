744f99ec0809832168cc495c2779b8eda6a18715
Wancheng Piao <pwc628@gmail.com>
2017-01-12 14:01:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,19 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  }, 
+  push: {
+      android: {
+        senderId: '...',
+        apiKey: '...'
+      },
+      ios: {
+        pfx: 'Certificates.p12',
+        passphrase: '', // optional password to your p12/PFX
+        bundleId: 'com.LavishTechnology.TattleType',
+        production: false
+      }
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
