6ebb42aa0a336e2e85d48648e86ff22dc9f7a5e5
Wancheng Piao <pwc628@gmail.com>
2017-01-12 15:32:14 +0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,23 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.define('push', function (request, response) {
+    // THIS METHOD NO LONGER WORKS
+    // Parse.Cloud.useMasterKey();
+
+    Parse.Push.send({
+        channels: request.params.channels,
+        data: request.params.data
+    }, {
+        // ADD THE `useMasterKey` TO THE OPTIONS OBJECT
+        useMasterKey: true,
+        success: function () {
+            response.success('Success!');
+        },
+        error: function (error) {
+            response.error('Error! ' + error.message);
+        }
+    });
+});
