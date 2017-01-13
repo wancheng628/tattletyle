a6a31c045f126634bca1db5b08d2244dcd0d611b
Wancheng Piao <pwc628@gmail.com>
2017-01-13 12:12:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,21 @@ var api = new ParseServer({
         bundleId: 'com.LavishTechnology.TattleType',
         production: true
       }
+    },
+  verifyUserEmails: true,
+  publicServerURL: 'https://tattletype.herokuapp.com/parse/',
+  appName: 'TattleType',
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      // The address that your emails come from
+      fromAddress: 'parse@example.com',
+      // Your domain from mailgun.com
+      domain: 'example.com',
+      // Your API key from mailgun.com
+      apiKey: 'key-c0b5ddf82cb2f4767b5e46a3a0a1c662',
     }
+  },
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
