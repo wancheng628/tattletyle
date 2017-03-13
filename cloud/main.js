bd80a3cc131de3a49fe7ef3dadb035164346122e
Wancheng Piao <pwc628@gmail.com>
2017-03-13 13:53:50 +0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -93,3 +93,29 @@ Parse.Cloud.define('pushTo', function (request, response) {
         }
     });
 });
+
+Parse.Cloud.beforeSave(Parse.Installation, function(request, response) {
+  Parse.Cloud.useMasterKey();
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("owner", request.user);
+  query.equalTo("uniqueID", request.object.get("uniqueID"));
+  query.first().then(function(duplicate) {
+      if (typeof duplicate === "undefined") {
+          console.log("Duplicate does not exist,New installation");
+          response.success();
+      } else {
+          console.log("Duplicate exist..Trying to delete " + duplicate.id);
+          duplicate.destroy().then(function(duplicate) {
+              console.log("Successfully deleted duplicate");
+              response.success();
+          }, function() {
+              console.log(error.code + " " + error.message);
+              response.success();
+          });
+
+      }
+  }, function(error) {
+      console.warn(error.code + error.message);
+      response.success();
+  });
+});
